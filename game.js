2ead48ec32a81d934eec49670b22bc514b7306fd
Are Sundnes <aresundnes@Ares-MacBook-Pro.local>
2017-10-27 16:08:32 +0200

    More styling stuff for stupid iOS safari

--- game.js
+++ game.js
@@ -71,7 +71,16 @@ function OnLoad() {
         
         document.body.addEventListener('touchmove',function(e){
 			e.preventDefault();
-	  });
+		  });
+	  
+		var lastTouchEnd = 0;
+		document.addEventListener('touchend', function (event) {
+		    var now = (new Date()).getTime();
+		    if (now - lastTouchEnd <= 300) {
+		        event.preventDefault();
+		    }
+		    lastTouchEnd = now;
+		}, false);	  
         
 }
 
